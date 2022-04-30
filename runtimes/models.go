a4fb2d096d5e6856664aa4afabb9e6f1bae186e1
a2tt <usera2tt@gmail.com>
2022-04-30 19:40:48 +0900

    feat: add endpoint returning supported langages

--- runtimes/models.go
+++ runtimes/models.go
@@ -9,10 +9,10 @@ type RuntimeImage struct {
 	ID           int64     `json:"id" gorm:"primaryKey"`
 	Name         string    `json:"name" gorm:"not null"`
 	LanguageName string    `json:"language_name" gorm:"not null"` // ForeignKey
-	Taskdef      string    `json:"taskdef" gorm:"not null"`       // AWS ECS Task Definition name
-	Revision     string    `json:"revision" gorm:"not null"`      // Revision number of Task Definition
+	Taskdef      string    `json:"-" gorm:"not null"`       // AWS ECS Task Definition name
+	Revision     string    `json:"-" gorm:"not null"`      // Revision number of Task Definition
 	Available    bool      `json:"available" gorm:"not null;default:0"`
-	CreatedAt    time.Time `json:"created_at" gorm:"not null"`
+	CreatedAt    time.Time `json:"-" gorm:"not null"`
 }
 
 type SupportedLanguage struct {
@@ -20,4 +20,10 @@ type SupportedLanguage struct {
 	Order int8   `json:"order" gorm:"not null;default:0"`
 
 	RuntimeImages []RuntimeImage `json:"images" gorm:"constraint:OnDelete:CASCADE;foreignKey:LanguageName;references:Name"`
+}
+
+func GetRuntimeImage(name, languageName string) RuntimeImage {
+	ri := RuntimeImage{Name: name, LanguageName: languageName}
+	db.DB.Where(&ri).Find(&ri)
+	return ri
 }
--- runtimes/routers.go
+++ runtimes/routers.go
@@ -1,1 +1,32 @@
 package runtimes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/together-coding/runtime-bridge/db"
+	"gorm.io/gorm/clause"
+	"net/http"
+)
+
+func Register(router *gin.RouterGroup) {
+	router.GET("/available", SupportedLang)
+}
+
+type SupportedLangResp struct {
+	Language []SupportedLanguage `json:"language"`
+}
+
+// SupportedLang godoc
+// @Summary  Return supported languages available
+// @Description Return supported languages available
+// @Tags     runtimes
+// @Produce  json
+// @Success  200 {object} SupportedLangResp
+// @Router   /runtimes/available [get]
+func SupportedLang(c *gin.Context) {
+	var languages []SupportedLanguage
+	db.DB.Preload(clause.Associations).Order("`order` ASC").Find(&languages)
+
+	c.JSON(http.StatusOK, SupportedLangResp{
+		Language: languages,
+	})
+}
