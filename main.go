6ecc6f752969e69c93fe2938057904e7bd310d19
a2tt <usera2tt@gmail.com>
2022-04-30 19:45:05 +0900

    chore: register routers and middlewares

--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	"github.com/together-coding/runtime-bridge/containers"
 	"github.com/together-coding/runtime-bridge/db"
+	"github.com/together-coding/runtime-bridge/docs"
+	"github.com/together-coding/runtime-bridge/runtimes"
+	"github.com/together-coding/runtime-bridge/users"
+	"github.com/together-coding/runtime-bridge/utils"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
+
 	// Load configs
-	dotenvErr := godotenv.Load(".env")
-	if dotenvErr != nil {
-		panic(dotenvErr)
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
 	}
 
 	// Initialize DB
@@ -18,6 +29,25 @@ func main() {
 
 	// Gin
 	r := gin.Default()
+	utils.CORSMiddleware(r)
 
+	// users
+	userApi := r.Group("/api/users")
+	userApi.Use(users.IdentifyUser())
+	users.Register(userApi.Group("/"))
+
+	// runtimes
+	runtimeApi := r.Group("/api/runtimes")
+	runtimes.Register(runtimeApi.Group("/"))
+
+	// containers
+	containerApi := r.Group("/api/containers")
+	containers.Register(containerApi.Group("/"), users.IdentifyUser)
+
+	// swagger
+	if utils.GetConfigBool("DEBUG") {
+		docs.SwaggerInfo.BasePath = "/api"
+		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 	r.Run()
 }
